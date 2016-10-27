72b56fc863e89d0d8c066e4cd985c49dfe1426da
JoshTPT <josh@tptech.com.au>
2016-10-27 11:44:40 +1000

    Initial commit

---
+++ tptBackup/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace tptBackup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::tptBackup.Properties.Resources.tpt_800px;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(203, 123);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(219, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 59);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Backup";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(229, 76);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(229, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(226, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Backup Target:";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Removable-Drive-USB.png");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start Backup";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Doing nothing...";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Enabled = false;
+            this.progressBar1.Location = new System.Drawing.Point(15, 154);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(443, 16);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 186);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(443, 171);
+            this.textBox1.TabIndex = 7;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 368);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TPTech Backup";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ tptBackup/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Management;
+
+namespace tptBackup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public List<string> usbDrives = new List<string>();
+        public int debugSize = 406;
+        public int normalSize = 219;
+        public string extDrive = "";
+        public string extDir = "\\Backup";
+        public string sourceDir = "C:\\";
+        public string logDir = Path.GetDirectoryName(Application.ExecutablePath) + "\\logs";
+        public string cfgFile = Path.GetDirectoryName(Application.ExecutablePath) + "\\tptbackup.ini";
+        public bool logging = true;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(cfgFile))
+            {
+                textBox1.AppendText(getTs() + " " + "Reading config from: " + cfgFile + Environment.NewLine);
+                var cfg = new IniFile(cfgFile);
+                if (cfg.KeyExists("extDir", "tptBackup")) { extDir = (string)cfg.Read("extDir", "tptBackup"); }
+                if (cfg.KeyExists("extDrive", "tptBackup")) { extDrive = (string)cfg.Read("extDrive", "tptBackup"); }
+                if (cfg.KeyExists("sourceDir", "tptBackup")) { sourceDir = (string)cfg.Read("sourceDir", "tptBackup"); }
+                if (cfg.KeyExists("logDir", "tptBackup")) { logDir = (string)cfg.Read("logDir", "tptBackup"); }
+            }
+
+            textBox1.AppendText(getTs() + "Source: " + sourceDir + Environment.NewLine);
+            textBox1.AppendText(getTs() + "Target Dir: " + extDir + Environment.NewLine);
+
+            if (!Directory.Exists(logDir))
+            {
+                try
+                {
+                    textBox1.AppendText(getTs() + " Log directory does not exist. Attempting to create." + Environment.NewLine);
+                    DirectoryInfo di = Directory.CreateDirectory(logDir);
+                }
+                catch (Exception ex)
+                {
+                    logging = false;
+                    textBox1.AppendText(getTs() + "Unable to create log directory. Logging is now disabled. " + logDir + Environment.NewLine);
+                }
+                finally
+                {
+                    textBox1.AppendText(getTs() + "Logging is enabled." + Environment.NewLine);
+                }
+            }
+            Height = normalSize;
+            comboBox1.Items.Clear();
+            int i;
+            if (extDrive != "")
+            {
+                usbDrives.Add(extDrive);
+                comboBox1.Items.Add(extDrive + " (manually added)");
+                textBox1.AppendText(getTs() + "Using manually specified drive" + Environment.NewLine);
+                i = 1;
+            }
+            else { i = 0; }
+            /*foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.DriveType == DriveType.Removable)
+                {
+                    drive.VolumeLabel
+                    comboBox1.Items.Add(drive.Name + " (" + drive.VolumeLabel + ")");
+                    usbDrives.Add(drive.Name);
+                    i++;
+                }
+            }*/
+            foreach (ManagementObject device in new ManagementObjectSearcher(@"SELECT * FROM Win32_DiskDrive WHERE InterfaceType LIKE 'USB%'").Get())
+            {
+                Console.WriteLine((string)device.GetPropertyValue("DeviceID"));
+                Console.WriteLine((string)device.GetPropertyValue("PNPDeviceID"));
+
+                foreach (ManagementObject partition in new ManagementObjectSearcher(
+                    "ASSOCIATORS OF {Win32_DiskDrive.DeviceID='" + device.Properties["DeviceID"].Value
+                    + "'} WHERE AssocClass = Win32_DiskDriveToDiskPartition").Get())
+                {
+                    foreach (ManagementObject disk in new ManagementObjectSearcher(
+                                "ASSOCIATORS OF {Win32_DiskPartition.DeviceID='"
+                                    + partition["DeviceID"]
+                                    + "'} WHERE AssocClass = Win32_LogicalDiskToPartition").Get())
+                    {
+                        /*foreach (PropertyData prop in disk.Properties)
+                        {
+                            textBox1.AppendText(getTs() + " " + prop.Name + ": " + prop.Value + Environment.NewLine);
+                        }*/
+                        textBox1.AppendText(getTs() + "Found USB HDD: " + disk["Name"] + " (" + disk["VolumeName"] + ")" + Environment.NewLine);
+                        comboBox1.Items.Add(disk["Name"] + " (" + disk["VolumeName"] + ")");
+                        usbDrives.Add((string)disk["Name"]);
+                        i++;
+                    }
+                }
+            }
+            if (i < 1)
+            {
+                MessageBox.Show("Unable to detect a USB hard drive. Please plug in the device and run this software again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+            }
+            else
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            bool bHandled = false;
+            switch (keyData)
+            {
+                case Keys.F5:
+                    if (Height == debugSize)
+                    {
+                        Height = normalSize;
+                    }
+                    else
+                    {
+                        Height = debugSize;
+                    }
+                    break;
+            }
+            return bHandled;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Ready to start backup?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                extDrive = usbDrives[comboBox1.SelectedIndex];
+                textBox1.AppendText(getTs() + "Backup starting..." + Environment.NewLine);
+                textBox1.AppendText(getTs() + "Target: " + extDrive + extDir + Environment.NewLine);
+                label3.Text = "Backing up... this may take some time..";
+                progressBar1.Style = ProgressBarStyle.Marquee;
+                progressBar1.Enabled = true;
+
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        public string getTs()
+        {
+            DateTime dt = DateTime.Now;
+            return "[" + dt.ToString() + "] ";
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "C:\\windows\\system32\\robocopy.exe";
+            if (!Directory.Exists(extDrive + extDir))
+            {
+                try
+                {
+                    textBox1.AppendText(getTs() + " Target directory does not exist. Attempting to create." + Environment.NewLine);
+                    DirectoryInfo di = Directory.CreateDirectory(logDir);
+                }
+                catch (Exception ex)
+                {
+                    logging = false;
+                    textBox1.AppendText(getTs() + "Unable to create target directory. Aborting! " + Environment.NewLine);
+                    MessageBox.Show("Cannot create target directory! " + Environment.NewLine + Environment.NewLine + extDrive + extDir, "Fatal error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    e.Cancel = true;
+                    Close();
+                }
+            }
+
+            if (!e.Cancel)
+            {
+                p.StartInfo.Arguments = sourceDir + " " + extDrive + extDir + " /MIR /FFT /R:0 /W:0 /Z /NP /NDL";
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.EnableRaisingEvents = true;
+                p.StartInfo.CreateNoWindow = true;
+
+                p.OutputDataReceived += new DataReceivedEventHandler((s, de) => { try { textBox1.AppendText(getTs() + de.Data + Environment.NewLine); label3.Text = de.Data.Replace("\t", " "); } catch (Exception ex) { } });
+                p.ErrorDataReceived += new DataReceivedEventHandler((s, de) => { try { textBox1.AppendText(getTs() + de.Data + Environment.NewLine); label3.Text = de.Data.Replace("\t", " "); } catch (Exception ex) { } });
+                p.Exited += new EventHandler((s, de) =>
+                {
+                    DateTime dt = DateTime.Now;
+                    string logF = "tptbackup_" + dt.Ticks.ToString() + ".log";
+                    File.WriteAllText(logDir + "\\" + logF, textBox1.Text);
+                    MessageBox.Show("Backup has been completed!", "Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    label3.Text = "Backup complete!";
+                    Close();
+                });
+
+                p.Start();
+                p.BeginOutputReadLine();
+                p.WaitForExit();
+            }
+        }
+    }
+}
